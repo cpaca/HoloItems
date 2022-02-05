78892091598f66729dc16f208189c2f846483b5c
TraceL <kivlanbahmidt@gmail.com>
2022-02-05 20:55:00 +0700

    Finalize AcquireCommand
    FInish AcquireCommand.java

--- src/main/java/xyz/holocons/mc/holoitemsrevamp/command/subcommand/AcquireCommand.java
+++ src/main/java/xyz/holocons/mc/holoitemsrevamp/command/subcommand/AcquireCommand.java
@@ -51,11 +51,78 @@ public class AcquireCommand implements SubCommand {
 
     @Override
     public boolean execute(CommandSender sender, String[] args) {
-        if(!(sender instanceof Player) || args.length == 0)
-            return true;
-        Player player = (Player) sender;
-        ItemStack item = plugin.getCollectionManager().getAllItems().get(args[0]).buildStack(player);
-        player.getInventory().addItem(item);
+        String itemName;
+        short amount;
+        Player player;
+
+        if (args.length >= 1) { // There is at least 1 argument (<item>).
+            itemName = args[0];
+            if (!plugin.getCollectionManager().getAllItems().containsKey(itemName)) {
+                sender.sendMessage(Component.text(args[0] + " is not a valid item!", NamedTextColor.YELLOW)
+                    .decoration(TextDecoration.ITALIC, true));
+                return false;
+            }
+        } else { // There is no argument.
+            return false;
+        }
+
+        if (args.length >= 2) { // There is at least 2 argument (<item> [amount]).
+            try {
+                amount = Short.parseShort(args[1]);
+                // 128 is the limit for `amount`, because I'm afraid of how many loops there are that depends on
+                // `amount` in this code.
+                if (amount <= 0 || amount > 128)
+                    throw new NumberFormatException();
+            } catch (NumberFormatException e) {
+                sender.sendMessage(Component.text(args[1] + " is not a valid number! It might be too high/low!",
+                        NamedTextColor.YELLOW)
+                    .decoration(TextDecoration.ITALIC, true));
+                return false;
+            }
+        } else { // There is less than 2 arguments. Set amount to default value (1).
+            amount = 1;
+        }
+
+        if (args.length >= 3) { // There is at least 3 argument (<item> [amount] [player]).
+            player = Bukkit.getPlayer(args[2]);
+            if (player == null) {
+                sender.sendMessage(Component.text(args[2] + " is not a valid player!", NamedTextColor.YELLOW)
+                    .decoration(TextDecoration.ITALIC, true));
+                return false;
+            }
+        } else { // There is less than 3 arguments. Check if the player to give the item to is the command sender.
+            if (!(sender instanceof Player)) {
+                sender.sendMessage(Component.text("Player not found! Did you forget to pick a player?",
+                        NamedTextColor.YELLOW).decoration(TextDecoration.ITALIC, true));
+                return false;
+            } else {
+                player = (Player) sender;
+            }
+        }
+
+        CustomItem customItem = plugin.getCollectionManager().getAllItems().get(args[0]);
+        ItemStack item = customItem.buildStack(player);
+
+        if (customItem.isStackable()) {
+            item.setAmount(amount);
+            Map<Integer, ItemStack> leftoverItems = player.getInventory().addItem(item);
+            // If there are items that could not be added to the player's inventory, drop it by their feet.
+            if (!leftoverItems.isEmpty()) {
+                for (ItemStack droppedItem : leftoverItems.values()) {
+                    player.getWorld().dropItemNaturally(player.getLocation(), droppedItem);
+                }
+            }
+        } else {
+            while (amount > 0) {
+                if (player.getInventory().firstEmpty() != -1) { // There is still an empty slot.
+                    player.getInventory().addItem(item.clone());
+                } else { // There is no slots left in the inventory, drop it by their feet.
+                    player.getWorld().dropItemNaturally(player.getLocation(), item.clone());
+                }
+                amount--;
+            }
+        }
+        EventCache.fullCache(player);
         return true;
     }
 }
