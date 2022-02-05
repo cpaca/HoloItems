21bcedfad87352db3d67179a87b7da44b160e93e
TraceL <kivlanbahmidt@gmail.com>
2022-02-05 20:49:27 +0700

    Add default error message for commands
    When a subcommand returns false in their execute method, the command format will be sent to the sender.

--- src/main/java/xyz/holocons/mc/holoitemsrevamp/command/MainCommand.java
+++ src/main/java/xyz/holocons/mc/holoitemsrevamp/command/MainCommand.java
@@ -1,5 +1,8 @@
 package xyz.holocons.mc.holoitemsrevamp.command;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -11,7 +14,9 @@ import xyz.holocons.mc.holoitemsrevamp.HoloItemsRevamp;
 import xyz.holocons.mc.holoitemsrevamp.command.subcommand.AcquireCommand;
 import xyz.holocons.mc.holoitemsrevamp.command.subcommand.CollectionsCommand;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class MainCommand implements CommandExecutor, TabCompleter {
@@ -29,18 +34,24 @@ public class MainCommand implements CommandExecutor, TabCompleter {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        if(args.length == 0){
+        if (args.length == 0){
             sender.sendMessage(ChatColor.DARK_AQUA + "=====" + ChatColor.GREEN + "HoloItems" + ChatColor.DARK_AQUA + "=====");
-            for(SubCommand subCommand : subCommands){
+            for (SubCommand subCommand : subCommands) {
                 sender.sendMessage("/holoitems " + ChatColor.AQUA + subCommand.getName() + " " + subCommand.getFormat() + ChatColor.GREEN +
                         " " + subCommand.getDesc());
             }
             sender.sendMessage(ChatColor.DARK_AQUA + "===================");
             return true;
         } else {
-            for(SubCommand subCommand : subCommands){
-                if(args[0].equalsIgnoreCase(subCommand.getName())){
-                    return subCommand.execute(sender, Arrays.copyOfRange(args, 1, args.length));
+            for (SubCommand subCommand : subCommands){
+                if (args[0].equalsIgnoreCase(subCommand.getName())){
+                    if (!subCommand.execute(sender, Arrays.copyOfRange(args, 1, args.length))) {
+                        sender.sendMessage(Component.text("/holoitems " + subCommand.getName() + " " + subCommand.getFormat())
+                            .decoration(TextDecoration.BOLD, true)
+                            .decoration(TextDecoration.UNDERLINED, true)
+                            .color(NamedTextColor.RED));
+                    }
+                    return true;
                 }
             }
         }
