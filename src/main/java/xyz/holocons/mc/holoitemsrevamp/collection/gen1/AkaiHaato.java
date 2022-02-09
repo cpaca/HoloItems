ae952e23ac35e7e3a21f8f6072f3affd2f892318
TraceL <kivlanbahmidt@gmail.com>
2022-02-10 01:12:37 +0700

    Fix Akai Haato's base64 texture

--- src/main/java/xyz/holocons/mc/holoitemsrevamp/collection/gen1/AkaiHaato.java
+++ src/main/java/xyz/holocons/mc/holoitemsrevamp/collection/gen1/AkaiHaato.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class AkaiHaato extends Idol {
 
     private static final String name = "akaihaato";
-    private static final String base64 = "ewogICJ0aW1lc3RhbXAiIDogMTYyMDE4NDUzMzY1OSwKICAicHJvZmlsZUlkIiA6ICJmMDNhNWE4ODZlY2M0OTMwYWUyMTA4MWI5MjYzNDhmZCIsCiAgInByb2ZpbGVOYW1lIiA6ICJBa2FpSGFhdG8iLAogICJzaWduYXR1cmVSZXF1aXJlZCIgOiB0cnVlLAogICJ0ZXh0dXJlcyIgOiB7CiAgICAiU0tJTiIgOiB7CiAgICAgICJ1cmwiIDogImh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZTc2M2ZjYzY1NzQ5NDBjODNiMTIwY2M2ZGUxNmQ1YzhhZWU0YzRlMzRjMzA0M2E5M2FlZThhN2U3NGFjMWVkNCIsCiAgICAgICJtZXRhZGF0YSIgOiB7CiAgICAgICAgIm1vZGVsIiA6ICJzbGltIgogICAgICB9CiAgICB9CiAgfQp9=";
+    private static final String base64 = "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZTc2M2ZjYzY1NzQ5NDBjODNiMTIwY2M2ZGUxNmQ1YzhhZWU0YzRlMzRjMzA0M2E5M2FlZThhN2U3NGFjMWVkNCJ9fX0=";
 
     public AkaiHaato() {
         super(name, base64);
@@ -30,4 +30,3 @@ public class AkaiHaato extends Idol {
         return null;
     }
 }
-            
