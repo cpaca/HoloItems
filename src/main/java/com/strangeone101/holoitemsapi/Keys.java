3ee01f906192108d59066b2afee71a15ad2f0ec3
Derek Lee <derek.evilcornrules@gmail.com>
2022-07-09 04:47:13 -0700

    Further refactor Keys

--- src/main/java/com/strangeone101/holoitemsapi/Keys.java
+++ src/main/java/com/strangeone101/holoitemsapi/Keys.java
@@ -1,6 +1,8 @@
 package com.strangeone101.holoitemsapi;
 
-import org.bukkit.NamespacedKey;
+import org.bukkit.Bukkit;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.Plugin;
 import java.util.UUID;
 
@@ -12,9 +14,100 @@ public class Keys {
     public static Property<String> ITEM_ID;
 
     public static void fillKeys(Plugin plugin) {
-        OWNER = new Properties.Owner(new NamespacedKey(plugin, "owner"));
-        COOLDOWN = new Properties.Cooldown(new NamespacedKey(plugin, "cooldown"));
-        UNSTACKABLE = new Properties.Unstackable(new NamespacedKey(plugin, "unstackable"));
-        ITEM_ID = new Properties.ItemId(new NamespacedKey(plugin, "item_id"));
+        OWNER = new Property<UUID>(plugin, "owner") {
+
+            @Override
+            public boolean has(PersistentDataContainer data) {
+                return data.has(getKey(), DataType.UUID);
+            }
+
+            @Override
+            public UUID get(PersistentDataContainer data) {
+                return data.get(getKey(), DataType.UUID);
+            }
+
+            @Override
+            public void set(PersistentDataContainer data, UUID value) {
+                data.set(getKey(), DataType.UUID, value);
+            }
+
+            @Override
+            public String getPropertyName() {
+                return "Owner";
+            }
+        };
+
+        COOLDOWN = new Property<Long>(plugin, "cooldown") {
+
+            @Override
+            public boolean has(PersistentDataContainer data) {
+                return data.has(getKey(), PersistentDataType.LONG);
+            }
+
+            @Override
+            public Long get(PersistentDataContainer data) {
+                return data.getOrDefault(getKey(), PersistentDataType.LONG, 0L);
+            }
+
+            @Override
+            public void set(PersistentDataContainer data, Long value) {
+                data.set(getKey(), PersistentDataType.LONG, value);
+            }
+
+            @Override
+            public String getPropertyName() {
+                return "Cooldown";
+            }
+        };
+
+        UNSTACKABLE = new Property<Boolean>(plugin, "unstackable") {
+
+            @Override
+            public boolean has(PersistentDataContainer data) {
+                return data.has(getKey(), PersistentDataType.INTEGER);
+            }
+
+            @Override
+            public Boolean get(PersistentDataContainer data) {
+                return has(data);
+            }
+
+            @Override
+            public void set(PersistentDataContainer data, Boolean value) {
+                if (value) {
+                    data.set(getKey(), PersistentDataType.INTEGER, Bukkit.getCurrentTick());
+                } else {
+                    data.remove(getKey());
+                }
+            }
+
+            @Override
+            public String getPropertyName() {
+                return "Unstackable";
+            }
+        };
+
+        ITEM_ID = new Property<String>(plugin, "item_id") {
+
+            @Override
+            public boolean has(PersistentDataContainer data) {
+                return data.has(getKey(), PersistentDataType.STRING);
+            }
+
+            @Override
+            public String get(PersistentDataContainer data) {
+                return data.get(getKey(), PersistentDataType.STRING);
+            }
+
+            @Override
+            public void set(PersistentDataContainer data, String value) {
+                data.set(getKey(), PersistentDataType.STRING, value);
+            }
+
+            @Override
+            public String getPropertyName() {
+                return "Item ID";
+            }
+        };
     }
 }
--- src/main/java/com/strangeone101/holoitemsapi/Property.java
+++ src/main/java/com/strangeone101/holoitemsapi/Property.java
@@ -3,9 +3,20 @@ package com.strangeone101.holoitemsapi;
 import org.bukkit.Keyed;
 import org.bukkit.NamespacedKey;
 import org.bukkit.persistence.PersistentDataContainer;
-import org.jetbrains.annotations.NotNull;
+import org.bukkit.plugin.Plugin;
 
 public abstract class Property<T> implements Keyed {
+
+    private final NamespacedKey key;
+
+    public Property(Plugin plugin, String key) {
+        this.key = new NamespacedKey(plugin, key);
+    }
+
+    @Override
+    public final NamespacedKey getKey() {
+        return key;
+    }
 
     public abstract boolean has(PersistentDataContainer data);
 
@@ -16,10 +27,7 @@ public abstract class Property<T> implements Keyed {
     public abstract String getPropertyName();
 
     @Override
-    public abstract @NotNull NamespacedKey getKey();
-
-    @Override
-    public int hashCode() {
+    public final int hashCode() {
         return getPropertyName().hashCode();
     }
 }
