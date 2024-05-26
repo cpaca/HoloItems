3e9d9ed036ed2c39cc3d4686a730278d5ef4968e
Tsumiki Miniwa <37684627+cpaca@users.noreply.github.com>
2024-05-25 23:05:10 -0700

    Create Splinter.java
    Didn't do any of the complicated stuff, just the one easy thing.

--- src/main/java/xyz/holocons/mc/holoitemsrevamp/enchantment/Splinter.java
+++ src/main/java/xyz/holocons/mc/holoitemsrevamp/enchantment/Splinter.java
@@ -1,4 +1,57 @@
 package xyz.holocons.mc.holoitemsrevamp.enchantment;
 
-public class Splinter {
+import com.destroystokyo.paper.MaterialTags;
+import com.strangeone101.holoitemsapi.enchantment.CustomEnchantment;
+import com.strangeone101.holoitemsapi.enchantment.EnchantmentAbility;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+import xyz.holocons.mc.holoitemsrevamp.HoloItemsRevamp;
+
+public class Splinter extends CustomEnchantment implements EnchantmentAbility {
+
+    private final HoloItemsRevamp plugin;
+
+    public Splinter(HoloItemsRevamp plugin){
+        super(plugin, "splinter");
+        this.plugin = plugin;
+    }
+
+    @Override
+    public int getMaxLevel() {
+        return 1;
+    }
+
+    @Override
+    public boolean conflictsWith(@NotNull Enchantment enchantment) {
+        return false;
+    }
+
+    @Override
+    public boolean canEnchantItem(@NotNull ItemStack itemStack) {
+        return MaterialTags.AXES.isTagged(itemStack);
+    }
+
+    @Override
+    public @NotNull Component displayName(int i) {
+        return Component.text()
+            .color(NamedTextColor.GRAY)
+            .decoration(TextDecoration.ITALIC, false)
+            .append(Component.text("Splinter"))
+            .build();
+    }
+
+    @Override
+    public int getCostMultiplier() {
+        return 12;
+    }
+
+    @Override
+    public void onBlockBreak(BlockBreakEvent event, ItemStack itemStack) {
+        // TODO.
+    }
 }
