8852047f165e775b6d15188e944b5b5d3e5b760a
Tsumiki Miniwa <37684627+cpaca@users.noreply.github.com>
2024-05-25 23:01:38 -0700

    Create SplinterBook
    Copied a lot of stuff from MagnetBook, honestly.

---
+++ src/main/java/xyz/holocons/mc/holoitemsrevamp/enchantment/Splinter.java
@@ -0,0 +1,4 @@
+package xyz.holocons.mc.holoitemsrevamp.enchantment;
+
+public class Splinter {
+}
---
+++ src/main/java/xyz/holocons/mc/holoitemsrevamp/item/SplinterBook.java
@@ -0,0 +1,70 @@
+package xyz.holocons.mc.holoitemsrevamp.item;
+
+import com.strangeone101.holoitemsapi.enchantment.CustomEnchantment;
+import com.strangeone101.holoitemsapi.enchantment.EnchantManager;
+import com.strangeone101.holoitemsapi.enchantment.Enchantable;
+import com.strangeone101.holoitemsapi.item.CustomItem;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.Recipe;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.EnchantmentStorageMeta;
+import xyz.holocons.mc.holoitemsrevamp.HoloItemsRevamp;
+
+import java.util.List;
+
+public class SplinterBook extends CustomItem implements Enchantable {
+    private static final String name = "splinter";
+    private static final Material material = Material.ENCHANTED_BOOK;
+    private static final Component displayName = Component.text("Splinter", NamedTextColor.RED);
+    private static final List<Component> lore = List.of(
+        Component.text("Chop down large areas of logs, leaves, or mushrooms!", NamedTextColor.DARK_PURPLE)
+    );
+
+    private final EnchantManager enchantManager;
+
+    public SplinterBook(HoloItemsRevamp plugin){
+        super(plugin, name, material, displayName, lore);
+        this.enchantManager = plugin.getEnchantManager();
+        this.register();
+    }
+
+    @Override
+    protected Recipe getRecipe() {
+        // Hmm. Due to getRecipe only returning one recipe, I can only have one orientation.
+        // Gonna call the recipe "left" for left-orientation, incase we ever decide to add right-orientation.
+        final var left = new ShapedRecipe(getKey(), buildStack(null));
+        left.shape(
+            "**",
+            "*&",
+            " |"
+        );
+        left.setIngredient('*', Material.SCUTE);
+        left.setIngredient('&', Material.CONDUIT);
+        left.setIngredient('|', Material.STICK);
+        return left;
+    }
+
+    @Override
+    public Enchantment getEnchantment() {
+        return CustomEnchantment.getByKey(getKey());
+    }
+
+    @Override
+    public ItemStack applyEnchantment(ItemStack itemStack) {
+        var enchantedStack = itemStack.clone();
+        var enchantedMeta = (EnchantmentStorageMeta) enchantedStack.getItemMeta();
+
+        if (enchantedMeta.addStoredEnchant(getEnchantment(), 1, false)) {
+            enchantedStack.setItemMeta(enchantedMeta);
+            enchantManager.removeCustomEnchantmentLore(enchantedStack);
+            enchantManager.applyCustomEnchantmentLore(enchantedStack);
+            return enchantedStack;
+        } else {
+            return null;
+        }
+    }
+}
