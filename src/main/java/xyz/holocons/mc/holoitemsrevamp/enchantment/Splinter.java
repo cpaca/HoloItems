19cc7a10529a957e00e34cf33892565c0243ccde
Tsumiki Miniwa <37684627+cpaca@users.noreply.github.com>
2024-06-02 00:39:23 -0700

    semicolon

--- src/main/java/xyz/holocons/mc/holoitemsrevamp/enchantment/Splinter.java
+++ src/main/java/xyz/holocons/mc/holoitemsrevamp/enchantment/Splinter.java
@@ -137,7 +137,7 @@ public class Splinter extends CustomEnchantment implements EnchantmentAbility {
     }
 
     private boolean isInvalidSplinterLocation(Location loc){
-        return Integrations.WORLDGUARD.canUseEnchantment(loc, Splinter.class)
+        return Integrations.WORLDGUARD.canUseEnchantment(loc, Splinter.class);
     }
 
     private static boolean isCompatibleMaterial(Material material) {
