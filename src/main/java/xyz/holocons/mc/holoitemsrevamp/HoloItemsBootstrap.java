26eb5b8beceb58db98d0a22404d9b4a15499f797
Tsumiki Miniwa <37684627+cpaca@users.noreply.github.com>
2025-09-17 01:02:39 -0700

    Fix bootstrap name

--- src/main/java/xyz/holocons/mc/holoitemsrevamp/HoloItemsBootstrap.java
+++ src/main/java/xyz/holocons/mc/holoitemsrevamp/HoloItemsBootstrap.java
@@ -5,9 +5,8 @@ import io.papermc.paper.plugin.bootstrap.PluginBootstrap;
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
 import io.papermc.paper.registry.RegistryKey;
 import org.jetbrains.annotations.NotNull;
-import xyz.holocons.mc.holoitemsrevamp.command.MainCommand;
 
-public class HoloitemsBootstrap implements PluginBootstrap {
+public class HoloItemsBootstrap implements PluginBootstrap {
     @Override
     public void bootstrap(@NotNull BootstrapContext context) {
         var manager = context.getLifecycleManager();
