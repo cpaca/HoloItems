c2b38e2291556e42bd0eaf09bc4e024e2cce3fb3
Tsumiki Miniwa <37684627+cpaca@users.noreply.github.com>
2025-09-18 17:17:54 -0700

    Remove ProtocolLib dependency
    No longer being used.

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,6 @@ repositories {
 
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
-    compileOnly("com.comphenix.protocol:ProtocolLib:5.4.0-SNAPSHOT")
     compileOnly("com.sk89q.worldguard:worldguard-bukkit:7.0.9")
     implementation("com.github.stefvanschie.inventoryframework:IF:0.11.3")
     implementation("com.typesafe:config:1.4.4")
@@ -94,8 +93,6 @@ paper {
     prefix = "HoloItems"
 
     serverDependencies {
-        register("ProtocolLib")
-
         register("WorldGuard") {
             required = false
         }
